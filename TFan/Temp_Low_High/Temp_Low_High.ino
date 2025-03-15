500d15fd3710ae22497695d316a7dd04969ca434
Hein Htet Zaw <30510357+mine969@users.noreply.github.com>
2025-03-15 19:50:16 +0700

    Update Temp_Low_High.ino

--- TFan/Temp_Low_High/Temp_Low_High.ino
+++ TFan/Temp_Low_High/Temp_Low_High.ino
@@ -1,11 +1,14 @@
 #include "DHT.h"
-
+#define RELAY D0       
 
 DHT dht(4, DHT11);
 
 void setup() {
     Serial.begin(115200);
     dht.begin();
+    pinMode(RELAY  , OUTPUT);
+    digitalWrite(RELAY  , LOW);
+
 }
 
 void loop() {
@@ -13,13 +16,16 @@ void loop() {
 
     if (isnan(t)) {  // Check if reading is valid
         Serial.println("Failed to read from DHT sensor!");
+        
         return;
     }
 
     if (t >= 28) {  // Correct comparison
         Serial.println("Temperature High");
+         digitalWrite(RELAY, LOW)
     } else {
         Serial.println("Temperature: LOW");
+         digitalWrite(RELAY, HIGH)
     }
 
     delay(2000);  // Delay to prevent excessive readings
