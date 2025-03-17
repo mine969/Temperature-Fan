d14ca0f8f4969507c8de2b4fbd48903d14357911
Hein Htet Zaw <30510357+mine969@users.noreply.github.com>
2025-03-17 13:30:24 +0700

    s

--- TFan/Temp_Low_High/Temp_Low_High.ino
+++ TFan/Temp_Low_High/Temp_Low_High.ino
@@ -1,4 +1,6 @@
 #include "DHT.h"
+#include <ESP8266WiFi.h>
+WiFiServer server(80);
 #define RELAY D0       
 
 DHT dht(4, DHT11);
@@ -8,7 +10,19 @@ void setup() {
     dht.begin();
     pinMode(RELAY  , OUTPUT);
     digitalWrite(RELAY  , LOW);
-
+    WiFi.begin("RSU-IOT", "1otRSU@6F00d");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+      delay(500);     Serial.print(".");
+    }
+    Serial.println("WiFi connected");
+    server.begin();  // Starts the Server
+    Serial.println("Server started");
+    Serial.print("IP Address of network: "); // will IP address on Serial Monitor
+    Serial.println(WiFi.localIP());
+    Serial.print("Copy and paste the following URL: https://"); // Will print IP address in URL format
+    Serial.print(WiFi.localIP());
+    Serial.println("/");
 }
 
 void loop() {
@@ -22,10 +36,10 @@ void loop() {
 
     if (t >= 28) {  // Correct comparison
         Serial.println("Temperature High");
-         digitalWrite(RELAY, LOW)
+         digitalWrite(RELAY, LOW);
     } else {
         Serial.println("Temperature: LOW");
-         digitalWrite(RELAY, HIGH)
+         digitalWrite(RELAY, HIGH);
     }
 
     delay(2000);  // Delay to prevent excessive readings
